72a3c60e6b7d968c617652e013e003896138eaaf
Lana <tiushkievich00@mail.ru>
2022-11-30 13:30:24 +0100

    feat: add basic types & any

---
+++ 1 types.ts
@@ -0,0 +1,22 @@
+const isFetching: boolean = true;
+const isLoading: boolean = false;
+
+const int: number = 42;
+const float: number = 4.2;
+const num: number = 3e10;
+
+const message: string = "Hello Typescript";
+
+const numberArray: number[] = [1, 1, 2, 3, 5, 8, 13];
+const numberArray2: Array<number> = [1, 1, 2, 3, 5, 8, 13];
+
+const words: string[] = ["hello", "Typescript"];
+
+//Tuple
+const contact: [string, number] = ["Lana", 679090090];
+
+//Any
+let variable: any = 42;
+//...
+variable = "New  String";
+variable = [];
