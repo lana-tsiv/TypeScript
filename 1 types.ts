7315ae4c8296a3ca9b72c2631aea39d6af8d9eab
Lana <tiushkievich00@mail.ru>
2022-12-01 13:35:16 +0100

    feat: add functions & types

--- 1 types.ts
+++ 1 types.ts
@@ -20,3 +20,30 @@ let variable: any = 42;
 //...
 variable = "New  String";
 variable = [];
+
+// ===
+function sayMyName(name: string): void {
+  console.log(name);
+}
+sayMyName("Lana");
+
+//Never
+function throwError(message: string): never {
+  throw new Error(message);
+}
+
+function infinite(): never {
+  while (true) {}
+}
+
+// Type
+type Login = string;
+const login: Login = "admin";
+// const login2: Login = 2;
+
+type ID = string | number;
+const id1: ID = 1234;
+const id2: ID = "1234";
+// const id3: ID = true;
+
+type SomeType = string | null | undefined;
